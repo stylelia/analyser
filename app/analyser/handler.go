b9e982a0d51e45615ecab312a3484ac0c0ce5ae3
Artur Kondas <devilyoushy@gmail.com>
2021-09-16 12:28:50 +0100

    add cookbook functions and repo clonning bits

--- app/analyser/handler.go
+++ app/analyser/handler.go
@@ -1,11 +1,14 @@
 package analyser
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 type Handler struct {
@@ -42,15 +45,40 @@ func (h *Handler) handle() error {
 		return err
 	}
 
+	// TODO: Fetch latest commit from cache and test against
+
 	// Check cache for cookstyle for a given repo.
 	// If exists, check version - if equal and if commit sha equal to cache, leave app
+	ver, err := getLatestCookbook(h.Client)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Fetch latest cookstyle version from cache and test against
+	log.Printf("%v\n", ver) // compiler moans
 
 	// If not exists or version is different or sha is different, clone the repo
+	// TODO: This requires an access to a valid SSH key on the lambda
+	err = repo.clone()
+	if err != nil {
+		return err
+	}
 
 	// run 'cookstyle -a --format json'
+	out, err := runCookbook()
+	if err != nil {
+		return err
+	}
 
 	// If cookstyle finds a change, create a new branch 'styleila/cookstyle_<version>'
+	if out.Summary.OffenseCount > 0 {
+		err = createBranch(ver)
+		if err != nil {
+			return err
+		}
+	}
 	// If no change, update cache with cookstyle and default branch sha
+	// TODO: this can be performed when cache is ready
 
 	// Raise a PR for that change
 	// put in pr body nice message based on json response from cookstyle
@@ -62,6 +90,7 @@ func (h *Handler) handle() error {
 	return nil
 }
 
+// Repository structs
 type Repository struct {
 	Org           string
 	Name          string
@@ -77,10 +106,46 @@ type GetLastCommit struct {
 	Sha string `json:"sha"`
 }
 
+// Cookbook structs
 type GetCookbook struct {
 	Version string `json:"version"`
 }
 
+// Cookbook check payload
+type CookbookCheck struct {
+	Metadata Metadata `json:"metadata"`
+	Files    []Files  `json:"files"`
+	Summary  Summary  `json:"summary"`
+}
+
+type Metadata struct {
+	RubocopVersion string `json:"rubocop_version"`
+	RubyEngine     string `json:"ruby_engine"`
+	RubyVersion    string `json:"ruby_version"`
+	RubyPatchlevel string `json:"ruby_patchlevel"`
+	RubyPlatform   string `json:"ruby_platform"`
+}
+
+type Files struct {
+	Path     string     `json:"path"`
+	Offenses []Offenses `json:"offenses"`
+}
+
+type Offenses struct {
+	Severity    string `json:"severity"`
+	Message     string `json:"message"`
+	CopName     string `json:"cop_name"`
+	Corrected   bool   `json:"corrected"`
+	Correctable bool   `json:"correctable"`
+}
+
+type Summary struct {
+	OffenseCount       int `json:"offense_count"`
+	TargetFileCount    int `json:"target_file_count"`
+	InspectedFileCount int `json:"inspected_file_count"`
+}
+
+// Endpoints
 const (
 	mainApi     string = "https://api.github.com"
 	cookbookApi string = "https://rubygems.org/api/v1/versions/cookstyle/latest.json"
@@ -139,6 +204,50 @@ func (r *Repository) getLastCommit(client *http.Client) error {
 	return nil
 }
 
+func (r *Repository) clone() error {
+	repoUri := fmt.Sprintf("git@github.com:%s/%s.git", r.Org, r.Name)
+
+	err := exec.Command("git", "clone", repoUri).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createBranch(cookbookVersion string) error {
+	cmdMessage := fmt.Sprintf("stylelia/cookstyle_%s", cookbookVersion)
+
+	err := exec.Command("git", "branch", "-b", cmdMessage).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func runCookbook() (CookbookCheck, error) {
+	var (
+		c      CookbookCheck
+		cmdOut bytes.Buffer
+	)
+
+	cmd := exec.Command("cookstyle", "-a", "format", "json")
+	cmd.Stdout = &cmdOut
+
+	err := cmd.Run()
+	if err != nil {
+		return c, err
+	}
+
+	err = gob.NewDecoder(&cmdOut).Decode(&c)
+	if err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
+
 func getLatestCookbook(client *http.Client) (string, error) {
 	request, err := http.NewRequest(http.MethodGet, cookbookApi, nil)
 	if err != nil {
@@ -156,8 +265,6 @@ func getLatestCookbook(client *http.Client) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	log.Printf("Version: %+v", getCookbookVersion)
 
 	return getCookbookVersion.Version, nil
 }
